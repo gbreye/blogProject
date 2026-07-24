4e0ff0017ec1587d68573b9b3bd4636eab63de4c
gbreye <gbrdsmain@gmail.com>
2026-07-24 06:47:47 -0300

    create folder organization

---
+++ client/src/Body/body.jsx
@@ -0,0 +1,17 @@
+import useState from 'react';
+import './body.css';
+
+function Body() {
+    return (
+        <body>
+            <main>
+                <section className="recentPosts">
+                    <h2>Welcome to My Blog</h2>
+                    <p>This is the body of the blog.</p>
+                </section>
+            </main>
+        </body>
+    );
+}
+
+export default Body;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Header from './header/header.jsx'
+import Header from './Header/header.jsx'
+import Body from './Body/body.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
+    <Body />
   </StrictMode>,
 )
