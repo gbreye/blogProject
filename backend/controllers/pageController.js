d043aeb89e0605b84801b86fedbcf03e27f01ccb
GABRIEL PATRICIO DA SILVA <gabriel.silva598@aluno.cps.sp.gov.br>
2026-08-04 12:53:51 -0300

    created the main method to se pages

--- backend/controllers/pageController.js
+++ backend/controllers/pageController.js
@@ -1,6 +1,7 @@
 import User from '../models/User.js';
 import Page from '../models/Page.js';
 import { createClient } from '@supabase/supabase-js';
+import { Cursor } from 'mongoose';
 
 const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.VITE_SUPABASE_PUBLISHABLE_KEY);
 
@@ -71,4 +72,30 @@ async function getPage(req, res) {
     }
 }
 
+async function searchPages(req, res, db) {
+  try {
+    const cursor = db.collection('pages').find().limit(5);
+    const pages = []
+    const results = []
+  
+  if(!cursor) {
+    return res.status(404).json({mes: "banco de dados indisponivel ou aba pages não encontrada"})
+  }
+  for await (const pages of cursor) {
+    results.push({
+      id: page._id,
+      title: page.title,
+      subTitle: page.subTitle
+    });
+  }
+  if(results.length === 0){
+    return res.status(404).json({mes:'Erro em tentar requisitr info do usuario nenhuma pagina salva'})
+  }
+  return res.status(200).json({results})
+ }catch(error) {
+  console.log('erro', error)
+  return res.status(500).json({mes: 'Internal Server Error.'})
+ }
+
+}
 export default { savePage, getPage }
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,4 +1,36 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
 function Home() {
+    const [title, setTitle] = useState('');
+    const [subTitle, setSubTitle] = useState('');
+    const [elements, setElements] = useState([]);
+    useEffect(() => {
+    const verifyPage = async () => {
+      
+      try {
+        const response = await fetch('http://localhost:3000/createPage/verifyPage', {
+          method: 'GET',
+        });
+
+        if (!response.ok) {
+          alert('Erro ao buscar os dados do post');
+          console.log('Erro em enviar/receber as informações', response.statusText);
+          return;
+        }
+
+        const data = await response.json();
+        setPostData(data);
+        setTitle(data.title);
+        setSubTitle(data.subTitle);
+       
+    } catch(error) {
+        alert('Erro')
+    }
+
+    verifyPage();
+    
+  }, []);
     return (
         <div>
             <h2>Hello World</h2>
